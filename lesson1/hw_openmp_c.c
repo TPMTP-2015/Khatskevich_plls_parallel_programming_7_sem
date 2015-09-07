6195dfaa0dd056b95cbdb9396d760926613a29a9
AleksK <avkhatskevich@gmail.com>
2015-09-07 22:07:10 +0300

    added pthread realization of hw

--- lesson1/hw_openmp_c.c
+++ lesson1/hw_openmp_c.c
@@ -3,7 +3,7 @@
 #include <omp.h>
 
 int main( int argc, char** argv){
-    long long int summ = 0, i, *mass, n_threads, array_size ;
+    long long int summ, i, *mass, n_threads, array_size ;
     if ( argc != 3){
         return -1;
     }
@@ -12,8 +12,8 @@ int main( int argc, char** argv){
     sscanf( argv[2] , "%lld", &array_size);
     printf("nthreads = %lld   array_size = %lld\n", n_threads , array_size);
     omp_set_num_threads(n_threads);
+    
     mass =  ( long long int *) malloc(sizeof(long long int)* array_size);
-    mass[1] = 5;
     #pragma omp parallel for reduction (+ : summ)
     for( i = 0; i < array_size; i++){
         summ+=mass[i];
